664b8c796fc49b79766a29b62f5eb4ec26a8daf1
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2024-08-13 11:40:42 +0900

    データを初期化

---
+++ src/test/java/com/example/petmanagment/PetmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
