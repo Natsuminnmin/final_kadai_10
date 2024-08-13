63b9ad3f0527d55fb746e4a5ffd0e5af74de70e5
Natsuminmin <basnatubadlove_misuse@outlook.jp>
2024-08-13 12:09:45 +0900

    READ/CREATE機能（登録のみ）を実装

---
+++ src/main/java/com/example/petmanagment/PetResponse.java
@@ -0,0 +1,14 @@
+package com.example.petmanagment;
+
+public class PetResponse {
+
+    private String message;
+
+    public PetResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
